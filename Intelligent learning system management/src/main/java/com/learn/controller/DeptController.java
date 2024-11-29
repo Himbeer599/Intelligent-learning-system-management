2c59abdaa16fddc44cf5ea3782ae759772e6cca2
Nannan Zhao <dtuznn@gmail.com>
2024-11-29 08:58:50 +0100

    use @RequestMapping on top of DeptController class to replace each, but pay attention to GetMapping, which needs Id Parameter transfer

--- Intelligent learning system management/src/main/java/com/learn/controller/DeptController.java
+++ Intelligent learning system management/src/main/java/com/learn/controller/DeptController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/depts")
 @RestController
 public class DeptController {
     //面向接口形式进行遍历声明
@@ -15,35 +16,35 @@ public class DeptController {
     private DeptService deptService;
     //一定要与接口文档路径保持一致
 //    @RequestMapping(value = "/depts", method = RequestMethod.GET)
-    @GetMapping("/depts")
+    @GetMapping
     public Result list(){
         System.out.println("Fetch all department data");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
 
-    @DeleteMapping("/depts")
+    @DeleteMapping
     public Result delete(Integer id){
         System.out.println("Delete department according to id:"+ id);
         deptService.deleteById(id);
         return Result.success();
     }
     //add a new department
-    @PostMapping("/depts")
+    @PostMapping
     public Result save(@RequestBody Dept dept){
         System.out.println("new department, dept=" + dept);
         deptService.save(dept);
         return Result.success();
     }
     //revise a department (step1 query by Id from frontend, step2, revise(similar to add a new department)
-    @GetMapping("/depts/{id}")
+    @GetMapping("/{id}")
     public Result getById(@PathVariable Integer id){
         System.out.println("Fetch department by id:"+ id);
         Dept dept = deptService.getById(id);
         return Result.success(dept);
     }
 
-    @PutMapping("/depts")
+    @PutMapping
     public Result update(@RequestBody Dept dept){
         System.out.println("update department, dept=" + dept);
         deptService.update(dept);
